431a8d4db517467c244b47673ed7fa0350356cb5
saariii96 <srogenmoser96@gmail.com>
2025-04-16 01:34:47 +0200

    update sideeffects and merge

--- 03-scripts/gpt/05-merge-synonyms-sideeffects.R
+++ 03-scripts/gpt/05-merge-synonyms-sideeffects.R
@@ -2,7 +2,7 @@
 synonyms <- read.csv("02-data/gpt_synonyms_top50.csv")
 sideeffects <- read.csv("02-data/sideeffects_top50.csv")
 
-# Zusammenführen (inner join auf drug oder medicinal product)
+# Zusammenführen (inner join auf drug)
 combined_df <- merge(synonyms, sideeffects, by = "drug", all.x = TRUE)
 
 # Speichern
@@ -10,3 +10,4 @@ write.csv(combined_df, "02-data/combined_synonyms_sideeffects.csv", row.names = 
 
 # Vorschau
 head(combined_df)
+nrow(combined_df)
